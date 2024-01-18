e9e8ee87b6bc6d24b97a34debfda341e62298f05
navprayukti1 <navprayukti@gmail.com>
2024-01-18 12:32:56 +0530

    feat:mqtt

--- app/main.py
+++ app/main.py
@@ -15,7 +15,7 @@ load_dotenv()
 logger = setup_applevel_logger(__name__)
 
 
-RELAY_ID = os.getenv("RELAY_ID")
+RELAY_ROOM_NO = os.getenv("RELAY_ROOM_NO")
 
 MQTT_HOST = os.getenv("MQTT_HOST")
 
@@ -23,8 +23,8 @@ MQTT_USERNAME = os.getenv("MQTT_USER")
 
 MQTT_PASSWORD = os.getenv("MQTT_PASSWORD")
 
-if not RELAY_ID:
-    logger.error("Relay Id not found")
+if not RELAY_ROOM_NO:
+    logger.error("Relay Room no not found")
     exit()
 
 if not MQTT_HOST:
@@ -41,17 +41,17 @@ if not MQTT_PASSWORD:
 
 
 def format_topic_name(x):
-    return f"{RELAY_ID}-{x}"
+    return f"{RELAY_ROOM_NO}-{x}"
 
 
 
 
 class APP:
     def __init__(
-        self, relayId: str, mqtt_host: str, mqtt_username: str, mqtt_password: str
+        self, relayRoomNo: str, mqtt_host: str, mqtt_username: str, mqtt_password: str
     ):
         self.client = None
-        self.relayId = relayId
+        self.relayRoomNo = relayRoomNo
         self.mqtt_host = mqtt_host
         self.mqtt_username = mqtt_username
         self.mqtt_password = mqtt_password
@@ -62,7 +62,7 @@ class APP:
             publish_message(
                 client,
                 Topic.NODE_STATE,
-                {"relayId": self.relayId, "isOnline": True},
+                {"relayRoomNo": self.relayRoomNo, "isOnline": True},
                 qos=1,
             )
             client.subscribe(format_topic_name(Topic.SEND_OTP))
@@ -97,11 +97,11 @@ class APP:
 
     def start(self):
         try:
-            self.client = mqtt.Client(self.relayId)
+            self.client = mqtt.Client(self.relayRoomNo)
             self.client.will_set(
                 Topic.NODE_STATE,
                 payload=str(
-                    json.dumps({"relayId": self.relayId, "isOnline": False})
+                    json.dumps({"relayRoomNo": self.relayRoomNo, "isOnline": False})
                 ),
                 qos=2,
             )
@@ -121,5 +121,5 @@ class APP:
             self.start()
 
 
-app = APP(RELAY_ID, MQTT_HOST, MQTT_USERNAME, MQTT_PASSWORD)
+app = APP(RELAY_ROOM_NO, MQTT_HOST, MQTT_USERNAME, MQTT_PASSWORD)
 app.start()
