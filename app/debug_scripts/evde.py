7e9f1d8153571efb3a5e5c8c2e97b31ee40fff58
binoy638 <binoy638@gmail.com>
2024-03-25 22:54:56 +0530

    fix

---
+++ app/debug_scripts/evde.py
@@ -0,0 +1,69 @@
+from evdev import InputDevice, ecodes, categorize
+
+event_path = "/dev/input/event11"  # Replace X with the appropriate number for your keyboard
+
+keyboard = InputDevice(event_path)
+
+
+
+shift_pressed = False 
+
+keystrokes = ''
+
+def on_key_press(key):
+    global keystrokes, shift_pressed
+
+    if key == "KEY_ENTER":
+        if keystrokes == "#123#":
+            print("CODE PRESSED")
+        keystrokes = '' 
+
+    if key == "KEY_LEFTSHIFT" or key == "KEY_RIGHTSHIFT":
+         shift_pressed = True 
+         return
+
+    if shift_pressed and "KEY_3":
+         keystrokes = keystrokes + '#'
+         shift_pressed = False 
+         return
+
+    if shift_pressed and "KEY_8":
+         keystrokes = keystrokes + '*'
+         shift_pressed = False  
+         return
+    
+    if key == "KEY_BACKSPACE":
+        keystrokes = keystrokes[:-1]
+        return
+    
+    if key == "KEY_1":
+        keystrokes += '1'
+    elif key == "KEY_2":
+        keystrokes += '2'
+    elif key == "KEY_3":
+        keystrokes += '3'
+    elif key == "KEY_4":
+        keystrokes += '4'
+    elif key == "KEY_5":
+        keystrokes += '5'
+    elif key == "KEY_6":
+        keystrokes += '6'
+    elif key == "KEY_7":
+        keystrokes += '7'
+    elif key == "KEY_8":
+        keystrokes += '8'
+    elif key == "KEY_9":
+        keystrokes += '9'
+    elif key == "KEY_0":
+        keystrokes += '0'
+
+    shift_pressed = False
+    print(keystrokes)
+
+# Listen for events
+for event in keyboard.read_loop():
+    if event.type == ecodes.EV_KEY:
+        key_event = categorize(event)
+        if key_event.keystate == key_event.key_down:
+            # if key_event.keycode in target_keys:
+                on_key_press(key_event.keycode)
